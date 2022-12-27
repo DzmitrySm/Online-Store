5e81bd5bd2e1c8b121dfa3ac544335cdc89e7b4e
DzmitrySm <magicjoker@yandex.ru>
2022-12-28 00:25:58 +0300

    feat: add header, footer components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import { CardModel } from './components/model/index.js';
-import { CardView } from './components/view/index.js';
+import { CardView } from './components/view/cardView';
 import { CardController } from './components/controller/index.js';
+import { HeaderViewComponent } from './components/view/headerViewComponent';
+import { FooterViewComponent } from './components/view/footerViewComponent';
 
 function init() {
+    const header = new HeaderViewComponent();
+    document.body.append(header.getElement());
     const model = new CardModel();
     const view = new CardView();
     const cardController = new CardController(view, model);
     cardController.getAll();
+    const footer = new FooterViewComponent();
+    document.body.append(footer.getElement());
 }
 
 init();
